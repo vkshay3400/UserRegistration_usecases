40db519f8c56d89e1276f05f149d26070c0b757d
vkshay3400 <akshayp2208@gmail.com>
2020-02-29 18:14:30 +0530

    Added: Validate last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,8 +4,18 @@ echo "*************************User Registration*************************"
 
 # VALIDATION OF FIRST NAME
 pattern="^[A-Z][a-z]{2,}$"
-read -p "Enter a name: " firstName
+read -p "Enter a first name: " firstName
 if [[ $firstName =~ $pattern ]]
+then
+	echo "Valid"
+else
+	echo "Not Valid"
+fi
+
+# VALIDATION OF LAST NAME
+pattern="^[A-Z][a-z]{2,}$"
+read -p "Enter a last name: " lastName
+if [[ $lastName =~ $pattern ]]
 then
 	echo "Valid"
 else
